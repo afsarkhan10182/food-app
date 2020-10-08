0bcbd589b89e69a1c2bc4dd2419345783fd77fbc
Khan <AfsarK@nedbank.co.za>
2020-10-08 12:52:50 +0200

    some changes

--- Delivery/src/app/detail/detail.page.ts
+++ Delivery/src/app/detail/detail.page.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit,ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ServerService } from '../service/server.service';
-import { NavController,Platform,LoadingController,IonSlides,Events,AlertController,ToastController } from '@ionic/angular';
+import { NavController, Platform, LoadingController, IonSlides, Events, AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-detail',
@@ -8,34 +8,30 @@ import { NavController,Platform,LoadingController,IonSlides,Events,AlertControll
   styleUrls: ['detail.page.scss'],
 })
 export class DetailPage {
-	
-  data:any;
-  text:any;
-  status:number = 3;
-  constructor(public toastController: ToastController,public alertController: AlertController,public server : ServerService,private nav: NavController,public events: Events,public loadingController : LoadingController)
-  {
-      this.data 	= JSON.parse(localStorage.getItem('odata'));
-      this.status	= this.data.st;
+
+  data: any;
+  text: any;
+  status: number = 3;
+  constructor(public toastController: ToastController, public alertController: AlertController, public server: ServerService, private nav: NavController, public events: Events, public loadingController: LoadingController) {
+    this.data = JSON.parse(localStorage.getItem('odata'));
+    this.status = this.data.st;
   }
 
-  ionViewWillEnter()
-  {
-    if(localStorage.getItem('app_text') && localStorage.getItem('app_text') != undefined)
-    {
+  ionViewWillEnter() {
+    if (localStorage.getItem('app_text') && localStorage.getItem('app_text') != undefined) {
       this.text = JSON.parse(localStorage.getItem('app_text'));
     }
   }
 
-  ngOnInit()
-  {
-    
+  ngOnInit() {
+
   }
 
-  async presentAlertConfirm(id,status) {
+  async presentAlertConfirm(id, status) {
     const alert = await this.alertController.create({
       header: 'Confirm!',
       message: 'Are you sure?',
-      mode:'ios',
+      mode: 'ios',
       buttons: [
         {
           text: 'Cancel',
@@ -47,8 +43,8 @@ export class DetailPage {
         }, {
           text: 'Yes',
           handler: () => {
-            
-            this.startRide(id,status);
+
+            this.startRide(id, status);
 
           }
         }
@@ -58,29 +54,26 @@ export class DetailPage {
     await alert.present();
   }
 
-  async startRide(id,type)
-  {
+  async startRide(id, type) {
     const loading = await this.loadingController.create({
       message: 'Please wait...',
     });
     await loading.present();
 
-    this.server.startRide(id,type).subscribe((response:any) => {
-    
-    if(type == 5)
-    {
-    	this.presentToast("Ride Completed Successfully.");
+    this.server.startRide(id, type).subscribe((response: any) => {
 
-    	this.nav.navigateRoot('home');
-    }
-    else
-    {
-    	this.presentToast("Ride Start Successfully.");
-    }
+      if (type == 5) {
+        this.presentToast("Ride Completed Successfully.");
 
-    this.status = 4;
+        this.nav.navigateRoot('home');
+      }
+      else {
+        this.presentToast("Ride Start Successfully.");
+      }
 
-    loading.dismiss();
+      this.status = 4;
+
+      loading.dismiss();
 
     });
   }
@@ -89,17 +82,16 @@ export class DetailPage {
     const toast = await this.toastController.create({
       message: txt,
       duration: 3000,
-      position : 'top',
-      mode:'ios',
-      color:'dark'
+      position: 'top',
+      mode: 'ios',
+      color: 'dark'
     });
     toast.present();
   }
 
-  detail(odata)
-  {
+  detail(odata) {
     localStorage.setItem('odata', JSON.stringify(odata));
-    
+
     this.nav.navigateForward('/detail');
   }
 }
