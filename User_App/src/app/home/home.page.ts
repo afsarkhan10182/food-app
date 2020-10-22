db992af6d2617a3f5c238e2ae7f5553bd6159570
Khan <AfsarK@nedbank.co.za>
2020-10-22 23:22:52 +0200

    added capicator for back button

--- User_App/src/app/home/home.page.ts
+++ User_App/src/app/home/home.page.ts
@@ -1,22 +1,23 @@
-import { Component, OnInit,ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ServerService } from '../service/server.service';
-import { NavController,Platform,LoadingController,IonSlides,Events } from '@ionic/angular';
-
+import { NavController, Platform, LoadingController, IonSlides, Events, IonRouterOutlet } from '@ionic/angular';
+import { Plugins } from '@capacitor/core';
+const { App } = Plugins;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-	
-BannerOption = {
+
+  BannerOption = {
     initialSlide: 0,
     slidesPerView: 2.3,
     loop: true,
     centeredSlides: false,
-    autoplay:false,
+    autoplay: false,
     speed: 500,
-    spaceBetween:7,
+    spaceBetween: 7,
 
   }
 
@@ -25,9 +26,9 @@ BannerOption = {
     slidesPerView: 3.5,
     loop: false,
     centeredSlides: false,
-    autoplay:false,
+    autoplay: false,
     speed: 500,
-    spaceBetween:-20,
+    spaceBetween: -20,
 
   }
 
@@ -36,9 +37,9 @@ BannerOption = {
     slidesPerView: 1.4,
     loop: true,
     centeredSlides: false,
-    autoplay:false,
+    autoplay: false,
     speed: 800,
-    spaceBetween:-9,
+    spaceBetween: -9,
 
   }
 
@@ -47,165 +48,155 @@ BannerOption = {
     slidesPerView: 1.3,
     loop: false,
     centeredSlides: false,
-    autoplay:true,
+    autoplay: true,
     speed: 800,
-    spaceBetween:7,
+    spaceBetween: 7,
 
   }
 
-  city_name:any;
-  data:any;
-  fakeData = [1,2,3,4,5,6,7];
-  oldData:any;
+  city_name: any;
+  data: any;
+  fakeData = [1, 2, 3, 4, 5, 6, 7];
+  oldData: any;
   showLoading = false;
-  filterPress:any;
+  filterPress: any;
   hasSearch = false;
-  searchQuery:any;
-  count:any;
-  text:any;
-  order:any;
+  searchQuery: any;
+  count: any;
+  text: any;
+  order: any;
 
-  constructor(public server : ServerService,private nav: NavController,public events: Events)
-  {
-    
+  constructor(private platform: Platform,
+    public server: ServerService,
+    private nav: NavController,
+    public events: Events,
+    private routerOutlet: IonRouterOutlet
+  ) {
+    this.platform.backButton.subscribeWithPriority(-1, () => {
+      if (!this.routerOutlet.canGoBack()) {
+        App.exitApp();
+      }
+    });
   }
 
-  ionViewWillEnter()
-  {
-    if(localStorage.getItem('app_text'))
-    {
+  ionViewWillEnter() {
+    if (localStorage.getItem('app_text')) {
       this.text = JSON.parse(localStorage.getItem('app_text'));
     }
 
     this.city_name = localStorage.getItem('city_name');
-  
-    this.server.cartCount(localStorage.getItem('cart_no')+"?user_id="+localStorage.getItem('user_id')).subscribe((response:any) => {
+
+    this.server.cartCount(localStorage.getItem('cart_no') + "?user_id=" + localStorage.getItem('user_id')).subscribe((response: any) => {
 
       this.count = response.data;
       this.order = response.order;
 
-     });
+    });
 
-    this.loadData(localStorage.getItem('city_id')+"?ss=ss");
+    this.loadData(localStorage.getItem('city_id') + "?ss=ss");
 
   }
 
-  ngOnInit()
-  {
+  ngOnInit() {
     this.searchQuery = null;
-    this.hasSearch   = false;
+    this.hasSearch = false;
   }
 
-  nearBy()
-  {
+  nearBy() {
     this.data = null;
-    this.loadData(localStorage.getItem('city_id')+"?lat="+localStorage.getItem('current_lat')+"&lng="+localStorage.getItem('current_lng'));
+    this.loadData(localStorage.getItem('city_id') + "?lat=" + localStorage.getItem('current_lat') + "&lng=" + localStorage.getItem('current_lng'));
   }
 
-  async loadData(city_id)
-  {
+  async loadData(city_id) {
     var lid = localStorage.getItem('lid') ? localStorage.getItem('lid') : 0;
 
-    this.server.homepage(city_id+"&lid="+lid).subscribe((response:any) => {
+    this.server.homepage(city_id + "&lid=" + lid).subscribe((response: any) => {
 
-    this.data = response.data;
-    console.log(this.data);
-    this.text = response.data.text;
+      this.data = response.data;
+      console.log(this.data);
+      this.text = response.data.text;
 
-    this.events.publish('text', this.text);
-    this.events.publish('admin', response.data.admin);
+      this.events.publish('text', this.text);
+      this.events.publish('admin', response.data.admin);
 
-    localStorage.setItem('app_text', JSON.stringify(response.data.text));
-    localStorage.setItem('admin', JSON.stringify(response.data.admin));
+      localStorage.setItem('app_text', JSON.stringify(response.data.text));
+      localStorage.setItem('admin', JSON.stringify(response.data.admin));
 
     });
   }
 
-  search(ev)
-  {
+  search(ev) {
     var val = ev.target.value;
 
-    if(val && val.length > 0)
-    {
-      this.data      = null;
+    if (val && val.length > 0) {
+      this.data = null;
       this.hasSearch = val;
 
-      this.loadData(localStorage.getItem('city_id')+"?q="+val);
+      this.loadData(localStorage.getItem('city_id') + "?q=" + val);
     }
-    else
-    {
+    else {
       this.ngOnInit();
       this.hasSearch = false;
     }
   }
 
-  async dataFilter(type)
-  {
+  async dataFilter(type) {
     this.filterPress = type;
     await this.delay(1000);
     this.filterPress = null;
 
-    if(type == 1)
-    {
-      this.data.store.sort((a,b) => {
-    
+    if (type == 1) {
+      this.data.store.sort((a, b) => {
+
         return parseFloat(b.discount_value) - parseFloat(a.discount_value);
 
-        });
+      });
     }
-    else if(type == 2)
-    {
-      this.data.store.sort((a,b) => {
-    
+    else if (type == 2) {
+      this.data.store.sort((a, b) => {
+
         return parseFloat(a.delivery_time) - parseFloat(b.delivery_time);
 
-        });
+      });
     }
-    else if(type == 3)
-    {
-      this.data.store.sort((a,b) => {
-    
+    else if (type == 3) {
+      this.data.store.sort((a, b) => {
+
         return parseFloat(b.trending) - parseFloat(a.trending);
 
-        });
+      });
     }
-    else if(type == 4)
-    {
-        this.data.store.sort((a,b) => {
-    
+    else if (type == 4) {
+      this.data.store.sort((a, b) => {
+
         return parseFloat(b.id) - parseFloat(a.id);
 
-        });
+      });
     }
-    else if(type == 5)
-    {
-      this.data.store.sort((a,b) => {
-    
+    else if (type == 5) {
+      this.data.store.sort((a, b) => {
+
         return parseFloat(b.rating) - parseFloat(a.rating);
 
-        });
+      });
     }
-    else if(type == 6)
-    {
-      
+    else if (type == 6) {
+
     }
-    else if(type == 7)
-    {
-      
+    else if (type == 7) {
+
     }
   }
 
   async delay(ms: number) {
-    
-    return new Promise( resolve => setTimeout(resolve, ms) );
+
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-  bannerLink(offer)
-  {
-    if(offer.link)
-    {
+  bannerLink(offer) {
+    if (offer.link) {
       this.data = null;
-      this.loadData(localStorage.getItem('city_id')+"?banner="+offer.id);
+      this.loadData(localStorage.getItem('city_id') + "?banner=" + offer.id);
     }
   }
 
@@ -214,17 +205,15 @@ BannerOption = {
     this.loadData(localStorage.getItem('city_id'));
 
     setTimeout(() => {
-      
+
       event.target.complete();
     }, 2000);
   }
 
-  itemPage(storeData)
-  {
-    if(storeData.open)
-    {
+  itemPage(storeData) {
+    if (storeData.open) {
       localStorage.setItem('menu_item', JSON.stringify(storeData));
-    
+
       this.nav.navigateForward('/item');
     }
   }
